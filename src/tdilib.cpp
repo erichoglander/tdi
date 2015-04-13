78e35d54f31605f4052fdf0270be621d7d2c53a8
Eric Höglander <erichoglander@gmail.com>
2015-04-13 23:13:08 +0200

    Added tdilib

---
+++ src/tdilib.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include "tdilib.h"
+
+using namespace std;
+
+
+/*
+* REQUEST
+*/
+void HttpRequest::split() {
+
+}
+void HttpRequest::parseHeader() {
+
+}
+void HttpRequest::parse() {
+	
+}
+
+
+
+/*
+* RESPONSE
+*/
+HttpResponseHeader::HttpResponseHeader() {
+	protocol = "HTTP/1.1";
+	code = "200 OK";
+	server = "TDI/0.1 (linux)";
+	connection = "Keep-Alive";
+	content_type = "text/html; charset=utf-8";
+}
+
+string HttpResponseHeader::toString(int content_length) {
+
+	string str;
+
+	str = 
+		protocol+" "+code+"\r\n"+
+		"Server: "+server+"\r\n"+
+		"Connection: "+connection+"\r\n"+
+		"Content-type: "+content_type+"\r\n"+
+		"Content-Length: "+to_string(content_length);
+
+	return str;
+
+}
+
+
+string HttpResponse::toString() {
+	string str;
+	str = 
+		header.toString(document.length())+
+		"\r\n\r\n"+
+		document;
+	return str;
+}
---
+++ src/tdilib.h
@@ -0,0 +1,44 @@
+#ifndef TDILIB_H_
+#define TDILIB_H_
+
+using namespace std;
+
+
+class HttpRequest {
+	
+	public:
+		string full, header, body;
+		string host, method, path, query;
+
+		void split();
+		void parseHeader();
+		void parse();
+
+};
+
+
+class HttpResponseHeader {
+
+	public:
+		string protocol, code, server, connection, content_type;
+
+		// Constructor
+		HttpResponseHeader();
+
+		// Joins all header parameters into a string
+		string toString(int content_length);
+
+};
+
+class HttpResponse {
+	
+	public:
+		HttpResponseHeader header;
+		string document;
+
+		string toString();
+
+};
+
+
+#endif
